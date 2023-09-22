48ed81e182ce908ca0abb6b7a1b04fa07d9812f2
Lebogang <lebogangodeveloper@gmail.com>
2023-09-22 13:15:25 +0200

    Execute a command using Puppet

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,5 @@
 # Puppet manifest to kill a process named "killmenow"
 
 exec { 'killmenow':
-  command     => 'pkill -f "killmenow"',
-  path        => ['/bin', '/usr/bin'],
-  refreshonly => true,
-  onlyif      => 'pgrep -f "killmenow"',
+  command     => '/usr/bin/pkill killmenow'
 }
