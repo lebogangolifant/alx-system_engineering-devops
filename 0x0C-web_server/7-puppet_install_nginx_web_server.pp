db81d34e246f8fcf5843cf6d406ac8410994fd94
Lebogang <lebogangodeveloper@gmail.com>
2023-10-02 17:00:44 +0200

    Install Nginx web server with Puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,33 +1,23 @@
 # Install Nginx web server with Puppet
 
+# Automating project requirements using Puppet
+
 package { 'nginx':
-  ensure => 'installed',
+  ensure => installed,
 }
 
-# Ensure Nginx is running and enabled
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => Package['nginx'],
+file_line { 'install':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.github.com/lebogangolifant permanent;',
 }
 
-# Create the Hello World HTML page
 file { '/var/www/html/index.html':
   content => 'Hello World!',
-  require => Package['nginx'],
 }
 
-# Configure the Nginx default site to serve Hello World!
-file { '/etc/nginx/sites-available/default':
-  content => template('nginx/default.erb'),
+service { 'nginx':
+  ensure  => running,
   require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Enable the default Nginx site
-file_line { '/etc/nginx/sites-enabled/default':
-  ensure  => 'link',
-  target  => '/etc/nginx/sites-available/default',
-  after   => 'listen 80 default_server;',
-  line    => 'rewrite ^/redirect_me https://www.github.com/lebogangolifant permanent;',
 }
