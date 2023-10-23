0590a42ffc874695c6a3af131144935642f25b13
Lebogang <lebogangodeveloper@gmail.com>
2023-10-23 05:53:12 -0700

    Import Statement for sys
    This makes it clear that we're using the argv attribute from the sys module

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,57 +5,38 @@ and displays information about an employee's TODO list progress.
 """
 
 import requests
-import sys
+from sys import argv
 
+# Check if the correct number of arguments is provided
+if len(argv) != 2:
+    print("Usage: python3 task_0.py <employee_id>")
+    exit(1)
 
-def get_employee_progress(employee_id):
-    # Define the API base URL
-    api_url = 'https://jsonplaceholder.typicode.com'
+# Get the employee ID from the command line argument
+employee_id = argv[1]
 
-    # Construct the URLs for user and TODO data
-    user_uri = f'{api_url}/users/{employee_id}'
-    todo_uri = f'{user_uri}/todos'
+# Define the base API URL and the specific user and todo URLs
+api_url = 'https://jsonplaceholder.typicode.com'
+user_uri = f'{api_url}/users/{employee_id}'
+todo_uri = f'{user_uri}/todos'
 
-    # Fetch user data
-    user_response = requests.get(user_uri)
-    if user_response.status_code != 200:
-        print("User not found")
-        sys.exit(1)
-    user_data = user_response.json()
-    employee_name = user_data.get('name')
+# Retrieve user data from the API
+user_data = requests.get(user_uri).json()
+employee_name = user_data.get('name')
 
-    # Fetch user's TODO tasks
-    todo_response = requests.get(todo_uri)
-    if todo_response.status_code != 200:
-        print("Failed to fetch user's tasks")
-        sys.exit(1)
-    todo_data = todo_response.json()
+# Retrieve the user's TODO list data from the API
+todo_data = requests.get(todo_uri).json()
+total_tasks = len(todo_data)
+completed_tasks = sum(1 for task in todo_data if task["completed"])
 
-    # Calculate the number of completed tasks and the total number of tasks
-    total_tasks = len(todo_data)
-    completed_tasks = sum(1 for task in todo_data if task["completed"])
+# Display the employee's progress
+progress_str = (
+    f"Employee {employee_name} is done with tasks "
+    f"({completed_tasks}/{total_tasks}):"
+)
+print(progress_str)
 
-    return employee_name, completed_tasks, total_tasks, todo_data
-
-
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: python3 task_0.py <employee_id>")
-        sys.exit(1)
-
-    try:
-        employee_id = int(sys.argv[1])
-    except ValueError:
-        print("Invalid employee ID. Please provide a valid integer.")
-        sys.exit(1)
-    (employee_name, completed_tasks,
-     total_tasks, todo_data) = get_employee_progress(employee_id)
-
-    # Format and display the employee's progress
-    progress_str = "Employee {} is done with tasks({}/{}):"
-    print(progress_str.format(employee_name, completed_tasks, total_tasks))
-
-    # Display completed tasks
-    for task in todo_data:
-        if task.get('completed'):
-            print("\t" + task.get('title'))
+# Display the completed tasks
+for task in todo_data:
+    if task.get('completed'):
+        print(f"\t{task.get('title')}")
